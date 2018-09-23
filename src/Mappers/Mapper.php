2d9448555b0ca002d1fd3188f10759dfd82a87ed
Monster3D <monster3d@mail.ru>
2018-09-23 12:11:21 +0300

    Added mapper

---
+++ src/Mappers/Mapper.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Qiwi\Mappers;
+
+use Qiwi\Enums\BaseTypeEnum;
+
+/**
+ * Class Mapper
+ * @package Qiwi\Mappers
+ */
+class Mapper
+{
+    /**
+     * @param array $value
+     * @param string $name
+     * @return bool
+     */
+    protected static function isValid(array $value, string $name) : bool
+    {
+        $result = false;
+        if (isset($value[$name]) && !is_null($value[$name])) {
+            $result = true;
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param array $data
+     * @param string $name
+     * @param string $type
+     * @return mixed
+     */
+    protected static function getValue(array $data, string $name, string $type)
+    {
+        $result = null;
+        $value = $data[$name];
+
+        switch ($type) {
+            case BaseTypeEnum::INT:
+                $result = (int)$value;
+                break;
+            case BaseTypeEnum::STRING:
+                $result = (string)$value;
+                break;
+        }
+
+        return $result;
+    }
+}
---
+++ src/Mappers/PersonProfileMappers/UserInfoMapper.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Qiwi\Mappers\PersonProfileMappers;
+
+use Qiwi\Elements\PersonProfile\UserInfo;
+use Qiwi\Enums\BaseTypeEnum;
+use Qiwi\Mappers\Mapper;
+
+final class UserInfoMapper extends Mapper
+{
+    /**
+     * @param UserInfo $userInfo
+     * @param array $userInfoResponse
+     * @return callable
+     */
+    final public static function map(UserInfo &$userInfo, array $userInfoResponse) : callable
+    {
+        $mapper = function () use(&$userInfo, $userInfoResponse) {
+            if (self::isValid($userInfoResponse, 'defaultPayCurrency')) {
+                $userInfo->setDefaultPayCurrency(self::getValue($userInfoResponse, 'defaultPayCurrency', BaseTypeEnum::INT));
+            }
+
+            if (self::isValid($userInfoResponse, 'defaultPaySource')) {
+                $userInfo->setDefaultPaySource(self::getValue($userInfoResponse, 'defaultPaySource', BaseTypeEnum::INT));
+            }
+
+            if (self::isValid($userInfoResponse, 'email')) {
+                $userInfo->setEmail(self::getValue($userInfoResponse, 'email', BaseTypeEnum::STRING));
+            }
+
+            if (self::isValid($userInfoResponse, 'firstTxnId')) {
+                $userInfo->setFirstTxnId(self::getValue($userInfoResponse, 'firstTxnId', BaseTypeEnum::INT));
+            }
+
+            if (self::isValid($userInfoResponse, 'language')) {
+                $userInfo->setLanguage(self::getValue($userInfoResponse, 'language', BaseTypeEnum::STRING));
+            }
+
+            if (self::isValid($userInfoResponse, 'operator')) {
+                $userInfo->setOperator(self::getValue($userInfoResponse, 'operator', BaseTypeEnum::STRING));
+            }
+
+            if (self::isValid($userInfoResponse, 'phoneHash')) {
+                $userInfo->setPhoneHash(self::getValue($userInfoResponse, 'phoneHash', BaseTypeEnum::STRING));
+            }
+
+            if (self::isValid($userInfoResponse, 'promoEnabled')) {
+                $userInfo->setPromoEnabled(self::getValue($userInfoResponse, 'promoEnabled', BaseTypeEnum::STRING));
+            }
+        };
+
+        return $mapper;
+    }
+
+}
