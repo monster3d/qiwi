946982d1811d4532001f5f975b4c6a0cd090348d
Monster3D <monster3d@mail.ru>
2018-09-29 12:33:54 +0300

    Coveres unit test UserInfoMapper

--- tests/unit/Mappers/PersonProfileMappers/UserInfoMapperTest.php
+++ tests/unit/Mappers/PersonProfileMappers/UserInfoMapperTest.php
@@ -4,9 +4,24 @@ namespace Tests\unit\Mappers\PersonProfileMappers;
 
 use PHPUnit\Framework\TestCase;
 use Qiwi\Elements\PersonProfile\UserInfo;
+use Qiwi\Mappers\PersonProfileMappers\UserInfoMapper;
+use Tests\Fixtures\Fixture;
+use Tests\Fixtures\FixtureManager;
 
+/**
+ * Class UserInfoMapperTest
+ * @package Tests\unit\Mappers\PersonProfileMappers
+ */
 class UserInfoMapperTest extends TestCase
 {
+
+    /**
+     * @return Fixture
+     */
+    private function makeFixtureManager() : ?Fixture
+    {
+        return FixtureManager::make('PersonProfile', 'userInfo');
+    }
     /**
      * @return UserInfo
      */
@@ -15,5 +30,38 @@ class UserInfoMapperTest extends TestCase
         return new UserInfo();
     }
 
+    /**
+     * @return UserInfo
+     */
+    private function makeActualUserInfo() : UserInfo
+    {
+        $userInfo = new UserInfo();
+        $userInfo
+            ->setPhoneHash('lgsco87234f0287')
+            ->setOperator('Beeline')
+            ->setLanguage('string')
+            ->setFirstTxnId('10807097143')
+            ->setDefaultPaySource(7)
+            ->setDefaultPayCurrency(643);
 
+        return $userInfo;
+    }
+
+    /**
+     * @group unit
+     */
+    public function testMake_UserInfo_returnVoid()
+    {
+        // Arrange
+        $userInfo        = $this->makeUserInfo();
+        $fixtureManager  = $this->makeFixtureManager();
+        $userInfoFixture = $fixtureManager->getAsArray();
+        $actualUserInfo  = $this->makeActualUserInfo();
+
+        // Act
+        UserInfoMapper::make($userInfo, $userInfoFixture['userInfo'])();
+
+        // Assert
+        $this->assertEquals($actualUserInfo, $userInfo);
+    }
 }
