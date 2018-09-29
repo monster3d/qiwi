affbf8ef96b570cb1b0de9b29136a4b8efbe912f
Monster3D <monster3d@mail.ru>
2018-09-29 12:34:33 +0300

    Added file for identification info mapper

---
+++ src/Mappers/PersonProfileMappers/IdentificationInfoMapper.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Qiwi\Mappers\PersonProfileMappers;
+
+
+use Qiwi\Mappers\Mapper;
+
+/**
+ * Class IdentificationInfoMapper
+ * @package Qiwi\Mappers\PersonProfileMappers
+ */
+class IdentificationInfoMapper extends Mapper
+{
+
+}
