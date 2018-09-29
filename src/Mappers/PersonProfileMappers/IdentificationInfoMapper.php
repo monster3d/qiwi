2ebfb193583c232db3119c4a119363de75d572b8
Monster3D <monster3d@mail.ru>
2018-09-29 13:36:52 +0300

    Added new mapper

--- src/Mappers/PersonProfileMappers/IdentificationInfoMapper.php
+++ src/Mappers/PersonProfileMappers/IdentificationInfoMapper.php
@@ -3,13 +3,49 @@
 namespace Qiwi\Mappers\PersonProfileMappers;
 
 
+use Qiwi\Elements\PersonProfile\IdentificationInfo;
+use Qiwi\Enums\BaseTypeEnum;
+use Qiwi\Enums\IdentificationLevelEnum;
 use Qiwi\Mappers\Mapper;
 
 /**
  * Class IdentificationInfoMapper
  * @package Qiwi\Mappers\PersonProfileMappers
  */
-class IdentificationInfoMapper extends Mapper
+final class IdentificationInfoMapper extends Mapper
 {
+    public static function make(IdentificationInfo &$identificationInfo, array $identificationInfoResponse) : callable
+    {
+        $mapper = function () use($identificationInfo, $identificationInfoResponse) {
+            if (self::isValid($identificationInfoResponse, 'defaultPayCurrency')) {
+                $identificationInfo->setBankAlias(self::getValue($identificationInfoResponse, 'bankAlias', BaseTypeEnum::STRING));
+            }
 
+            if (self::isValid($identificationInfoResponse, 'defaultPaySource')) {
+                $identificationLevel         = null;
+                $identificationLevelResponse = self::getValue($identificationInfoResponse, 'identificationLevel', BaseTypeEnum::STRING);
+
+                switch ($identificationLevelResponse) {
+                    case IdentificationLevelEnum::ANONYMOUS:
+                        $identificationLevel = IdentificationLevelEnum::ANONYMOUS;
+                        break;
+                    case IdentificationLevelEnum::FULL:
+                        $identificationLevel = IdentificationLevelEnum::FULL;
+                        break;
+                    case IdentificationLevelEnum::SIMPLE:
+                        $identificationLevel = IdentificationLevelEnum::SIMPLE;
+                        break;
+                    case IdentificationLevelEnum::VERIFIED;
+                        $identificationLevel = IdentificationLevelEnum::VERIFIED;
+                        break;
+                }
+
+                if (!is_null($identificationLevel)) {
+                    $identificationInfo->setIdentificationLevel($identificationLevel);
+                }
+            }
+        };
+
+        return $mapper;
+    }
 }
