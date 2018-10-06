b6e8cce62b991506f5c71ed373f68721aee9c137
Monster3D <monster3d@mail.ru>
2018-10-06 11:03:18 +0300

    Added global settings

---
+++ src/Settings.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Qiwi;
+
+
+abstract class Settings
+{
+    /**
+     * Current remote api version
+     */
+    const GLOBAL_VERSION_API = 'v1';
+
+    /**
+     * This application version
+     */
+    const APPLICATION_VERSION = '1.0.0.000';
+}
