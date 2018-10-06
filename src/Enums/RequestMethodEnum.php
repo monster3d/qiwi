b05f4231f7bb3070cecdf2df1aba3666fe5a3a1d
Monster3D <monster3d@mail.ru>
2018-10-06 11:02:54 +0300

    Added enum request methods

---
+++ src/Enums/RequestMethodEnum.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Qiwi\Enums;
+
+/**
+ * Class RequestMethodEnum
+ * @package Qiwi\Enums
+ */
+class RequestMethodEnum
+{
+    /**
+     * Request method GET
+     */
+    const GET = 'GET';
+
+    /**
+     * Request method POST
+     */
+    const POST = 'POST';
+}
